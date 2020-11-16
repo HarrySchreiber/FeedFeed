949f8a67f91c0153832ab59002760fb2ebaa09ad
FortunEdge <fortunedge@gmail.com>
2020-11-15 22:15:27 -0500

    Base template for admin and admin home

--- FeedFeed/server.py
+++ FeedFeed/server.py
@@ -5,7 +5,34 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.urandom(32)
 
+#Settings for testing
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
 #TODO: Populate this with Logic to route to Login Screen or Home Screen depending on if the user is logged into an account
 @app.route("/")
 def root():
     return ""
+
+@app.route("/dash/")
+def adminHome():
+    #Until we get user logins and db implementation, placeholder data will be used for testing/viewing styling
+    return render_template("admin_home.html",
+                            username="Administrator",
+                            uiSectionName="Home",
+                            showBackButton=False)
+
+@app.route("/dash/meals/")
+def adminMealManager():
+    return render_template("admin_meals.html",
+                            username="Administrator",
+                            uiSectionName="Home",
+                            showBackButton=True,
+                            backLink=url_for("adminHome"))
+
+@app.route("/dash/ingredients/")
+def adminIngredientManager():
+    return render_template("admin_ingredients.html",
+                            username="Administrator",
+                            uiSectionName="Home",
+                            showBackButton=True,
+                            backLink=url_for("adminHome"))
