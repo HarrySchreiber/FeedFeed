53d8f1539dbab9f079b5d3d03441a5518b86c6e4
dybasjacob <dybasjt17@gcc.edu>
2020-11-16 17:31:50 -0500

    Added routes to all user pages

--- FeedFeed/server.py
+++ FeedFeed/server.py
@@ -1,9 +1,12 @@
 #TODO: I'm pretty sure this is how were going to host our app, but I could be jumping the gun so not sure we'll use this
 import os
 from flask import Flask, render_template, request, redirect, url_for, session, flash #TODO: Not sure all of these are nesessary yet, but well find out
+import sqlite3
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.urandom(32)
+
+conn = sqlite3.connect
 
 #TODO: Populate this with Logic to route to Login Screen or Home Screen depending on if the user is logged into an account
 @app.route("/")
@@ -13,3 +16,15 @@ def root():
 @app.route("/home/")
 def get_user_home():
     return render_template("user_home.html")
+
+@app.route("/mygoals/")
+def get_user_goals():
+    return render_template("user_goals.html")
+
+@app.route("/mypantry/")
+def get_user_pantry():
+    return render_template("user_pantry.html")
+
+@app.route("/dailyplan/")
+def get_daily_plan():
+    return render_template("user_daily_plan.html")
