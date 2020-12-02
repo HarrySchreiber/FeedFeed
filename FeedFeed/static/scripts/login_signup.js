f521a7d2bd39847f7ba1f7cb94351288ef3b948f
HarrySchreiber <44842528+HarrySchreiber@users.noreply.github.com>
2020-12-01 23:06:56 -0500

    Signup form now mostly validates except for character length

--- FeedFeed/static/scripts/login_signup.js
+++ FeedFeed/static/scripts/login_signup.js
@@ -1,8 +1,28 @@
 window.addEventListener("DOMContentLoaded", function() {
-    let login_button = document.getElementById("log-in-radio");
-    let signup_button = document.getElementById("sign-up-radio");
-    login_button.addEventListener("click",toggle_login);
-    signup_button.addEventListener("click",toggle_signup);
+    let login_toggle_button = document.getElementById("log-in-radio");
+    let signup_toggle_button = document.getElementById("sign-up-radio");
+    login_toggle_button.addEventListener("click",toggle_login);
+    signup_toggle_button.addEventListener("click",toggle_signup);
+    let signup_form = document.getElementById("signup-form");
+    signup_form.addEventListener("submit", (e) => {
+        let signup_password = document.getElementById("signup-password").value;
+        let signup_confirm_password = document.getElementById("signup-confirm-password").value;
+        let signup_email = document.getElementById("signup-email").value;
+        if(signup_password == "" || signup_confirm_password == "" || signup_email == ""){
+            e.preventDefault();
+            alert("Missing form field");
+        }
+
+        if(signup_password != signup_confirm_password){
+            e.preventDefault();
+            alert("Passwords Don't Match");
+        }
+
+        if(!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(signup_email)){
+            e.preventDefault();
+            alert("Email is Malformed");
+        }
+    });
 });
 
 function toggle_login(){
