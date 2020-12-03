efc8c10c6de74520514e44f90b0203aed3bfcd24
FortunEdge <fortunedge@gmail.com>
2020-12-02 19:20:43 -0500

    Sorting tables

--- FeedFeed/static/scripts/data_explorer_script.js
+++ FeedFeed/static/scripts/data_explorer_script.js
@@ -51,6 +51,14 @@ window.addEventListener("DOMContentLoaded", function() {
 
     document.getElementById("selectAll").addEventListener("change", toggleSelection);
     document.getElementById("deleteAll").addEventListener("click", showModal);
+
+    //Attatch listers to headers to sort by
+    let headers = document.getElementsByClassName("sortHeader");
+    for (header of headers) {
+        header.addEventListener("click", function() {
+            sortTable(parseInt(this.getAttribute("data-colNum")))
+        })
+    }
 });
 
 function deleteItem(elt) {
@@ -122,3 +130,75 @@ function toggleSelection() {
         }
     }
 }
+
+//Table sorting code from: https://www.w3schools.com/howto/howto_js_sort_table.asp
+function sortTable(n, isNum) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("data-table");
+    switching = true;
+    // Set the sorting direction to ascending:
+    dir = "asc";
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+      // Start by saying: no switching is done:
+      switching = false;
+      rows = table.rows;
+      /* Loop through all table rows (except the
+      first, which contains table headers): */
+      for (i = 1; i < (rows.length - 1); i++) {
+        // Start by saying there should be no switching:
+        shouldSwitch = false;
+        /* Get the two elements you want to compare,
+        one from current row and one from the next: */
+        x = rows[i].getElementsByTagName("TD")[n];
+        y = rows[i + 1].getElementsByTagName("TD")[n];
+        /* Check if the two rows should switch place,
+        based on the direction, asc or desc: */
+        if (dir == "asc") {
+          if(isNum) {
+            if (Number(x.innerHTML) > Number(y.innerHTML)) {
+                shouldSwitch = true;
+                break;
+            }
+          }
+          else {
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                // If so, mark as a switch and break the loop:
+                shouldSwitch = true;
+                break;
+            }
+          }
+        } else if (dir == "desc") {
+            if(isNum) {
+                if (Number(x.innerHTML) < Number(y.innerHTML)) {
+                    shouldSwitch = true;
+                    break;
+                }
+              }
+              else {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    // If so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+              }
+        }
+      }
+      if (shouldSwitch) {
+        /* If a switch has been marked, make the switch
+        and mark that a switch has been done: */
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        // Each time a switch is done, increase this count by 1:
+        switchcount ++;
+      } else {
+        /* If no switching has been done AND the direction is "asc",
+        set the direction to "desc" and run the while loop again. */
+        if (switchcount == 0 && dir == "asc") {
+          dir = "desc";
+          switching = true;
+        }
+      }
+    }
+  }
