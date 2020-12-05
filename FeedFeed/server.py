0c350cdc47ab351b72a82c1b88acd324ba1538cb
dybasjacob <dybasjt17@gcc.edu>
2020-12-05 11:02:13 -0700

    Added sample data to DB, fixed bug where generating daily plan would remove favorite meals, made large columns scrollable.

--- FeedFeed/server.py
+++ FeedFeed/server.py
@@ -905,13 +905,13 @@ def get_daily_plan():
         flash("Must Sign Into an Account")
         return redirect(url_for("login_get"))
     rows=[]
-    mealIngs=[]
+    mealIngs = []
     conn = sqlite3.connect("Database.db")
     c = conn.cursor()
     favMeals = c.execute('''
     SELECT * FROM FavoriteMeals JOIN Meal on FavoriteMeals.meal_id = Meal.id and FavoriteMeals.user_id = ?;
     ''', (session['uid'],))
-    return render_template("user_daily_plan.html", rows=rows, mealIngs=[], favMeals=favMeals)
+    return render_template("user_daily_plan.html", rows=rows, mealIngs=mealIngs, favMeals=favMeals)
 
 @app.route("/dailyplan/", methods=["POST"])
 def post_daily_plan():
@@ -920,23 +920,17 @@ def post_daily_plan():
     rows = c.execute(''' SELECT * FROM Meal; ''')
     conn.commit()
 
-    #conn2 = sqlite3.connect("Database.db")
-    #c2 = conn2.cursor()
     mealIngs = c.execute(''' 
     SELECT * FROM MealIngredients JOIN Ingredient ON MealIngredients.ingredient_id = Ingredient.id
      JOIN Meal ON Meal.id = MealIngredients.meal_id;
      ''').fetchall()
     conn.commit()
 
-    #conn3 = sqlite3.connect("Database.db")
-    #c3 = conn3.cursor()
     favMeals = c.execute('''
     SELECT * FROM FavoriteMeals JOIN Meal on FavoriteMeals.meal_id = Meal.id and FavoriteMeals.user_id = ?;
-    ''', (session['uid'],))
+    ''', (session['uid'],)).fetchall()
     conn.commit()
 
-    #conn4 = sqlite3.connect("Database.db")
-    #c4 = conn4.cursor()
     mealIds = c.execute(''' 
     SELECT * FROM MealIngredients;
     ''').fetchall()
@@ -945,11 +939,8 @@ def post_daily_plan():
         mealInfo[entry[0]] = []
     for entry in mealIds:
         mealInfo[entry[0]].append(entry[1])
-    print(mealInfo) #PERFECT
     conn.commit()
 
-    #conn5 = sqlite3.connect("Database.db")
-    #c5 = conn5.cursor()
     userIngredients = c.execute('''
     SELECT * FROM UserIngredients;
     ''').fetchall()
@@ -959,7 +950,6 @@ def post_daily_plan():
         userInfo[entry[0]] = []
     for value in userIngredients:
         userInfo[value[0]].append(value[1])
-    print(userInfo) #GOOD
     conn.commit()
 
     conn6 = sqlite3.connect("Database.db")
@@ -981,7 +971,6 @@ def post_daily_plan():
                 c6.execute('''
                 INSERT INTO UserMealOptions(user_id, meal_id) VALUES (?, ?)
                 ''', (user, meal))
-                print(f'user {user} can make meal {meal}')
     conn6.commit()
 
     userOptions = c6.execute('''
